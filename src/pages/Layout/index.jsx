6032b977f5b894a74f5a415cb25523dbfb514174
Kaozhi Chen <kylerchen16@gmail.com>
2024-04-04 20:20:47 -0400

    antd theme test

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Button } from 'antd-mobile';
 
 const Layout = () => {
   return (
     <div>
       <Outlet />
       Layout
+      <Button color='primary'>nihao</Button>
     </div>
   );
 };
