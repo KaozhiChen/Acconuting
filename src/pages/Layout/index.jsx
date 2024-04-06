ad2b62659b53fb23e0d8d5558b18a5e971a149f7
Kaozhi Chen <kylerchen16@gmail.com>
2024-04-06 09:45:03 -0400

    get billList

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Button } from 'antd-mobile';
+import { useDispatch } from 'react-redux';
+import { getBillList } from '../../store/modules/billStore';
 
 const Layout = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBillList());
+  }, [dispatch]);
   return (
     <div>
       <Outlet />
--- src/store/modules/billStore.jsx
+++ src/store/modules/billStore.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const billStore = createSlice({
   name: 'bill',
@@ -14,5 +15,12 @@ const billStore = createSlice({
 
 const { setBillList } = billStore.actions;
 
+const getBillList = () => {
+  return async (dispatch) => {
+    const res = await axios.get('http://localhost:8888/ka');
+    dispatch(setBillList(res.data));
+  };
+};
+export { getBillList };
 const reducer = billStore.reducer;
 export default reducer;
