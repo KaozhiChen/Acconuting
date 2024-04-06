596cf6051a9cf167e1263d823bfb9f27c2c336df
Kaozhi Chen <kylerchen16@gmail.com>
2024-04-06 10:13:24 -0400

    Tab bar

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,8 +1,32 @@
 import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Button } from 'antd-mobile';
 import { useDispatch } from 'react-redux';
 import { getBillList } from '../../store/modules/billStore';
+import { TabBar } from 'antd-mobile';
+import './index.scss';
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from 'antd-mobile-icons';
+
+const tabs = [
+  {
+    key: '/month',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+];
 
 const Layout = () => {
   const dispatch = useDispatch();
@@ -10,10 +34,17 @@ const Layout = () => {
     dispatch(getBillList());
   }, [dispatch]);
   return (
-    <div>
-      <Outlet />
-      Layout
-      <Button color='primary'>nihao</Button>
+    <div className='layout'>
+      <div className='container'>
+        <Outlet />
+      </div>
+      <div className='footer'>
+        <TabBar>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
     </div>
   );
 };
